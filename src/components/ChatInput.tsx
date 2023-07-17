e9aa037b32f0cb0ab41a4db9d91dc0975cf75b18
Espacio-Root <Abdullahd00124@gmail.com>
2023-07-17 08:50:13 +0300

    Debug 2

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -38,7 +38,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, chatId, ...props }) => {
     },
     onSuccess: async (stream : any) => {
       if (!stream) throw new Error('Stream Not Available')
-      console.log('debut', stream)
+      
       const id = nanoid()
       const responseMessage: Message = {
         chatId: chatId,
@@ -47,6 +47,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, chatId, ...props }) => {
         text: '',
       }
       addMessage(responseMessage)
+      console.log(messages)
       
       // const reader = stream.getReader();
       const decoder = new TextDecoder("utf-8");
